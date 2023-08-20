cf9f1ca18a2b6c76695681c0296ca40cbe7bf440
imonous <mykolassle@gmail.com>
2023-08-20 12:43:49 +0300

    Move core functionality to dedicated function

--- main.py
+++ main.py
@@ -12,6 +12,37 @@ def calc_err(expect, real):
     y = abs(expect[1] - real[1])
     return math.sqrt(x**2 + y**2)
 
+def trace_light(init_angle, height, width, parts):
+    t = 1e-30
+    x, y = 0, 0
+    angle = init_angle
+    v, v_old = 0, 0
+    init = True
+    for _ in range(PARTS - 1):
+        v = part_v(t)
+
+        # 1 / v * sin(angle) = 1 / v_new * sin(angle_new)
+        # new_angle = arcsin(v_new / v * sin(angle))
+        if not init:
+            w = v / v_old * math.sin(angle)
+            w = w % 1 if w != 1 else 1 # fix domain
+            angle = math.asin(w)
+
+        # cos(a) = height / long; long = height / cos(a)
+        d = (HEIGHT / PARTS) / math.cos(angle) 
+
+        # d = vt; t = d / v
+        t += d / v
+
+        # sin(a) = width / long; x += sin(a) * long; y += height
+        x += math.sin(angle) * d
+        y += HEIGHT / PARTS
+
+        if init:
+            init = False
+        v_old = v
+    return x, y
+
 
 if __name__ == "__main__":
     HEIGHT = 3
@@ -21,38 +52,9 @@ if __name__ == "__main__":
     min_err = math.inf
     # for init_angle in np.linspace(0, math.pi / 2, 100):
     for init_angle in [math.pi / 6]:
-        t = 1e-30
-        x, y = 0, 0
-        angle = init_angle
-        v, v_old = 0, 0
-        init = True
-        for _ in range(PARTS - 1):
-            v = part_v(t)
-
-            # 1 / v * sin(angle) = 1 / v_new * sin(angle_new)
-            # new_angle = arcsin(v_new / v * sin(angle))
-            if not init:
-                w = v / v_old * math.sin(angle)
-                w = w % 1 if w != 1 else 1 # fix domain
-                angle = math.asin(w)
-
-            # cos(a) = height / long; long = height / cos(a)
-            d = (HEIGHT / PARTS) / math.cos(angle) 
-
-            # d = vt; t = d / v
-            t += d / v
-
-            # sin(a) = width / long; x += sin(a) * long; y += height
-            x += math.sin(angle) * d
-            y += HEIGHT / PARTS
-
-            if init:
-                init = False
-            v_old = v
-
+        x, y = trace_light(init_angle, HEIGHT, WIDTH, PARTS)
         err = calc_err((WIDTH, HEIGHT), (x, y))  
         if err < min_err:
             min_err = err
-
     print(min_err)
 
