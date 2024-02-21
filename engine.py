ff919f3c4d84d9fc10e1fbe4db76dd0a16e35b77
imonous <mykolassle@gmail.com>
2024-02-21 01:47:57 +0100

    Demo ConstructBrach.

--- engine.py
+++ engine.py
@@ -24,7 +24,7 @@ class LightMedium:
         return f"LightMedium(v={self.v:.2e})"
 
 
-class BernoulliLightMedium(LightMedium):
+class BernoulliMedium(LightMedium):
     def __init__(self, y: float, g: float = 9.8067) -> None:
         """
         A light medium from the Johann Bernoulli indirect method for the brachistochrone
@@ -39,18 +39,18 @@ class BernoulliLightMedium(LightMedium):
                 v = sqrt(-2 * g * y).
         """
         self.v = math.sqrt(-2 * self.g * y)
+        # self.v = math.sqrt(2 * self.g * y)  # temporary
 
 
 class BernoulliRay:
-    def __init__(self, x: float, y: float) -> None:
+    def __init__(self, y: float, angle: float) -> None:
         """Light ray as a vector in the 1st cartesian quadrant. Angles are in radians.
         Specifically made for the experiment."""
-        if (x, y) == (0, 0):
-            raise ValueError("BernoulliRay cannot be a null vector.")
-        if x < 0 or y < 0:
-            raise ValueError("BernoulliRay must be in the first (cartesian) quadrant.")
-        self._x, self._y = x, y
+        # if (x, y) == (0, 0):
+        #     raise ValueError("BernoulliRay cannot be a null vector.")
         self.reflected = False
+        self.y = y
+        self.angle = angle
 
     # TODO x, y = 0, 0?
     @property
@@ -68,7 +68,7 @@ class BernoulliRay:
     @property
     def y(self) -> float:
         """Negate y if reflected."""
-        return -1 * self._y if self.reflected else self._y
+        return -self._y if self.reflected else self._y
 
     @y.setter
     def y(self, value: float) -> None:
@@ -82,7 +82,7 @@ class BernoulliRay:
     @property
     def angle(self) -> float:
         """Get the angle of the vector. Caching would add unnecessary complexity."""
-        return math.atan2(self.y, self.x)
+        return math.atan2(abs(self.y), self.x)
 
     @angle.setter
     def angle(self, value: float) -> None:
@@ -92,7 +92,7 @@ class BernoulliRay:
         """
         if value >= math.pi / 2 or value <= 0:
             raise ValueError("Angle outside of the domain.")
-        self.x = self.y / math.tan(value)
+        self.x = abs(self.y) / math.tan(value)
 
     def get_incidence(self) -> float:
         """
@@ -102,7 +102,6 @@ class BernoulliRay:
         """
         angle = self.angle
         return math.pi / 2 - angle
-        # return math.copysign(math.pi / 2 - abs(angle), angle)
 
     def propagate(self, medium1: type[LightMedium], medium2: type[LightMedium]) -> None:
         """
@@ -119,7 +118,7 @@ class BernoulliRay:
         alpha_1 = self.get_incidence()
         v1, v2 = medium1.v, medium2.v
         w = (v2 / v1) * math.sin(alpha_1)
-        if abs(w) >= 1 and v2 > v1:
+        if abs(w) >= 1 and v2 > v1:  # reflect
             self.reflected = True
         else:  # refract
             w = math.asin(w)
@@ -130,13 +129,28 @@ class BernoulliRay:
 
 
 class ConstructBrachistochrone:
-    def __init__(self):
+    def __init__(
+        self, init_angle: float = math.pi / 3, step_height: float = 1e-1
+    ) -> None:
         """Construct the Brachistochrone curve."""
-        pass
+        self.x, self.y = 0, 0
+        self.points = [(self.x, self.y)]
+        self.ray = BernoulliRay(step_height, init_angle)
+        self.step_height = step_height
 
-    def step(self):
+    def step(self) -> None:
         """Step forward."""
-        pass
+        try:
+            m1 = BernoulliMedium(self.y - self.ray.y)
+            m2 = BernoulliMedium(
+                self.y - 2 * self.ray.y
+            )  # shift forw by 1 for init err
+            self.ray.propagate(m1, m2)
+            self.x += self.ray.x
+            self.y -= self.ray.y
+            self.points.append((self.x, self.y))
+        except ValueError:
+            raise StopIteration("Final step reached.")
 
 
 # def trace_light(
