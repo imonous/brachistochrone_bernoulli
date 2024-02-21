425a0bf231bd03ddbc83a221355b0903f88804b1
imonous <mykolassle@gmail.com>
2024-02-21 13:15:26 +0100

    Improved demo.

--- app.py
+++ app.py
@@ -1,44 +1,51 @@
-import panel as pn
 import math
+import time
 
 import engine
 import analytics
 
+import panel as pn
 import holoviews as hv
+from holoviews.streams import Pipe
 
 pn.extension(design="material")
 
-ITERATIONS = 300000
-MEDIUM_HEIGHT = 0.5
+in_angle_slider = pn.widgets.FloatSlider(
+    name="Initial ray angle",
+    start=math.pi / 2.7,
+    end=math.pi / 2.2,
+    step=0.01,
+    value=math.pi / 2.4,
+)
+gconst_slider = pn.widgets.FloatSlider(
+    name="Gravitational Acceleration", start=1, end=20, step=1, value=10
+)
 
-# bokeh_pane = pn.pane.Bokeh(plot)
-bkc = hv.DynamicMap(analytics.bkc_plot, kdims=["angle", "g"])
-bkc = bkc.redim.range(angle=(math.pi / 2.7, math.pi / 2.2), g=(5, 20))
+pipe = Pipe(data=[])
+live_plot = hv.DynamicMap(hv.Curve, streams=[pipe])
 
 
-# angle_slider = pn.widgets.FloatSlider(
-#     name="Angle", start=0.5, end=89.5, step=0.5, value=45
-# )
+def stop_trace(event):
+    pass
 
-app = pn.Row(bkc)
+
+def trace_path(event):
+    bkc_data = engine.ConstructBrachistochrone(
+        init_angle=in_angle_slider.value, g=gconst_slider.value
+    )
+    while bkc_data.step():
+        time.sleep(0.5)
+        pipe.send(bkc_data.points)
+
+
+trace_path_btn = pn.widgets.Button(name="Trace path", button_type="primary")
+stop_trace_btn = pn.widgets.Button(name="Stop trace", button_type="primary")
+
+trace_path_btn.on_click(trace_path)
+stop_trace_btn.on_click(stop_trace)
+
+app = pn.Row(
+    pn.Column(pn.Row(trace_path_btn, stop_trace_btn), in_angle_slider, gconst_slider),
+    live_plot,
+)
 app.servable()
-
-# def plot(
-#     points: list[tuple[float, float]], medium_height: float, file_path=None, save=False
-# ) -> hv.core.layout.Layout:
-#     alim = np.max(np.abs(np.array(points)))
-#     # lines = hv.HLines(-np.arange(0, alim, medium_height)).opts(
-#     #     color="lightgray", line_width=1
-#     # )
-
-#     pad = 1e-1
-#     xlim = (-alim * pad, alim + alim * pad)
-#     ylim = (-alim - alim * pad, alim * pad)
-#     curve = hv.Curve(points).opts(xlim=xlim, ylim=ylim, height=650, width=650)
-
-#     # res = lines * curve
-#     res = curve
-#     if save:
-#         # show(hv.render(res))
-#         hv.save(res, file_path, fmt="png")
-#     return res
